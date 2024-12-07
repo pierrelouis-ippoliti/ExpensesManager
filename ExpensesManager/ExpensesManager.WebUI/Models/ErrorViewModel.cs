94f1e08dd87965ca8d86fbbc82375ba25d175a62
Pierre-Louis IPPOLITI <pierrelouisippoliti@gmail.com>
2024-12-07 17:39:35 +0100

    (1-build-app-clean-architecture) feat : creation of the app architecture

---
+++ ExpensesManager/ExpensesManager.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExpensesManager.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

51bfc8267f5291cd2556d8c38a643be849f89195
Pierre-Louis IPPOLITI <pierrelouisippoliti@gmail.com>
2024-12-15 20:12:41 +0100

    (10-add-test-coverage) feat: add test coverage to test project for Domain layer

---
+++ ExpensesManager/src/ExpensesManager.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpensesManager.Domain.Common
+{
+    public abstract class BaseAuditableEntity : BaseEntity<Guid>
+    {
+        public DateTimeOffset CreatedAt { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTimeOffset LastModifiedAt { get; set; }
+        public string LastModifiedBy { get; set; } = string.Empty;
+    }
+}
---
+++ ExpensesManager/tests/ExpensesManager.Domain.UnitTests/BastTests.cs
@@ -0,0 +1,20 @@
+﻿namespace ExpensesManager.Domain.UnitTests
+{
+    [TestFixture]
+    public class BastTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void VeryFirstTestOfDomain()
+        {
+            var test = true;
+
+            Assert.That(test, Is.True);
+        }
+    }
+}
